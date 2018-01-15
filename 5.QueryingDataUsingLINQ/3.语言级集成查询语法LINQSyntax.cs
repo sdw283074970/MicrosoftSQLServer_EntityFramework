17229beac92b28d124f954dd01afdca126b2d578
sdw283074970 <downwes@gmail.com>
2018-01-15 16:46:13 -0500

    Update 3.语言级集成查询语法LINQSyntax.cs

--- 5.QueryingDataUsingLINQ/3.语言级集成查询语法LINQSyntax.cs
+++ 5.QueryingDataUsingLINQ/3.语言级集成查询语法LINQSyntax.cs
@@ -105,6 +105,133 @@
         //Author Id: 3, Total Price: 45 Id为3的作者发起的课程总价为45
         //Author Id: 4, Total Price: 170  Id为4的作者发起的课程总价为170
 
-  //5.结合Join。
+  //5.结合Join。在SQL中我们有三种Join，分别是INNER JOIN、GROUP JOIN和CROSS JOIN。LINQ中的InnerJoin和CrossJoin与SQL中的很像，但在LINQ中使用
+    //GroupJoin有点像在SQL中使用LEFTJOIN。我们分开详解。
+    
+    //1）LINQ中的INNERJOIN。在SQL中，INNER JOIN为两个表的交集，在LINQ中也一样。假设项目需求为查询返回一张课程名和作者名在一起的表，
+      //格式为“课名 - 作者名”，我们就要用到INNER JOIN。代码和结果如下：
 
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
 
+            var qurey = from c in context.Courses
+                        join a in context.Authors on c.AuthorId equals a.Id   //选择两个表中所有AuthorId等于Id的条目为结果
+                        select new { AuthorName = a.Name, CourseName = c.Name };    //优化程序，将结果有用的部分投射到匿名类中
+
+            foreach (var x in qurey)  //迭代输出结果
+                Console.WriteLine("{0} - {1}", x.CourseName, x.AuthorName);
+        }
+      
+      //输出结果为:
+
+        //C# Basics - Mosh Hamedani
+        //C# Intermediate - Mosh Hamedani
+        //C# Advanced - Mosh Hamedani
+        //Javascript: Understanding the Weird Parts - Anthony Alicea
+        //Learn and Understand AngularJS - Anthony Alicea
+        //Learn and Understand NodeJS - Anthony Alicea
+        //Programming for Complete Beginners - Eric Wise
+        //A 16 Hour C# Course with Visual Studio 2013 - Tom Owsiak
+        //Learn JavaScript Through Visual Studio 2013 - Tom Owsiak
+
+      //以上为普通情况。如果两个表中有导向属性，即外键关系，则可不用关键词，直接输出结果。如Courses表和Authors表本来就有导航属性，即Courses表中
+        //的AuthorId字段直接指向了Authors表中的Id字段，则可直接输出以上需求，代码如下;
+
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var qurey = from c in context.Courses
+                        //因为AuthorId为从Courses表指向Author表的外键的关系，我们可以直接通过调用Courses表来取得Authors表中的引用
+                        select new { AuthorName = c.Author.Name, CourseName = c.Name };    //优化程序，将结果有用的部分投射到匿名类中
+
+            foreach (var x in qurey)  //迭代输出结果
+                Console.WriteLine("{0} - {1}", x.CourseName, x.AuthorName);
+        }//输出结果与上面是一样的
+
+    //2）LINQ中的GROUP JOIN。相当于SQL中的LEFT JOIN，经常会在实际生产中配合累计函数查询结果。如，我们要查询每个作者发起的课程总价值，结果中要返回
+      //作者的名字(不是GROUPBY例子中的Id)和总价格，就要用到GROUP JOIN。其中，由于Authors表和Courses表为一对多的关系，Authors表应该为左表，Courses
+      //为右表，代码和结果如下：
+
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var qurey = from a in context.Authors
+                        join c in context.Courses on a.Id equals c.AuthorId
+                        into g    //之前是标准的INNERJOIN，加上into后就变成GROUPJOIN，g为左表Authors的完全集
+                        select new { AuthorName = a.Name, TotalPrice = g.Sum(c => c.FullPrice) };
+
+            foreach (var x in qurey)
+                Console.WriteLine("{0} - {1}", x.AuthorName, x.TotalPrice);
+        }
+
+      //输出结果为(与GROUPBY例子结果相同)：
+
+        //Author Id: 1, Total Price: 167  Id为1的作者发起的课程总价为167
+        //Author Id: 2, Total Price: 397  Id为2的作者发起的课程总价为397
+        //Author Id: 3, Total Price: 45 Id为3的作者发起的课程总价为45
+        //Author Id: 4, Total Price: 170  Id为4的作者发起的课程总价为170
+
+    //3）LINQ中的CROSS JOIN。与SQL中的CROSS JOIN相同，返回左表条目*右边条目的结果。如过我们将Authors表和Courses表交叉结合，将产生一些没有意义的
+      //条目，如一个写C#的作者写了一本JS的书。在实际生产中，交叉结合的结果需要进一步筛选才能使用。如，项目需求为Authors表与Courses表交叉集合，返回
+      //一切作者和书名的结合可能，代码和输出结果如下：
+
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            var qurey = from a in context.Authors
+                        from c in context.Courses   //CROSSJOIN直接两个from关键词走起，然后投射即可
+                        select new { AuthorName = a.Name, CourseName = c.Name };
+
+            foreach (var x in qurey)    //迭代输出结果
+                Console.WriteLine("{0} - {1}", x.AuthorName, x.CourseName);
+        }
+
+      //输出结果为Authors*Courses的一切结合可能：
+
+        //Mosh Hamedani - Learn and Understand AngularJS
+        //Mosh Hamedani - Learn and Understand NodeJS
+        //Mosh Hamedani - Programming for Complete Beginners
+        //Mosh Hamedani - A 16 Hour C# Course with Visual Studio 2013
+        //Mosh Hamedani - Learn JavaScript Through Visual Studio 2013
+        //Anthony Alicea - C# Basics
+        //Anthony Alicea - C# Intermediate
+        //Anthony Alicea - C# Advanced
+        //Anthony Alicea - Javascript: Understanding the Weird Parts
+        //Anthony Alicea - Learn and Understand AngularJS
+        //Anthony Alicea - Learn and Understand NodeJS
+        //Anthony Alicea - Programming for Complete Beginners
+        //Anthony Alicea - A 16 Hour C# Course with Visual Studio 2013
+        //Anthony Alicea - Learn JavaScript Through Visual Studio 2013
+        //Eric Wise - C# Basics
+        //Eric Wise - C# Intermediate
+        //Eric Wise - C# Advanced
+        //Eric Wise - Javascript: Understanding the Weird Parts
+        //Eric Wise - Learn and Understand AngularJS
+        //Eric Wise - Learn and Understand NodeJS
+        //Eric Wise - Programming for Complete Beginners
+        //Eric Wise - A 16 Hour C# Course with Visual Studio 2013
+        //Eric Wise - Learn JavaScript Through Visual Studio 2013
+        //Tom Owsiak - C# Basics
+        //Tom Owsiak - C# Intermediate
+        //Tom Owsiak - C# Advanced
+        //Tom Owsiak - Javascript: Understanding the Weird Parts
+        //Tom Owsiak - Learn and Understand AngularJS
+        //Tom Owsiak - Learn and Understand NodeJS
+        //Tom Owsiak - Programming for Complete Beginners
+        //Tom Owsiak - A 16 Hour C# Course with Visual Studio 2013
+        //Tom Owsiak - Learn JavaScript Through Visual Studio 2013
+        //John Smith - C# Basics
+        //John Smith - C# Intermediate
+        //John Smith - C# Advanced
+        //John Smith - Javascript: Understanding the Weird Parts
+        //John Smith - Learn and Understand AngularJS
+        //John Smith - Learn and Understand NodeJS
+        //John Smith - Programming for Complete Beginners
+        //John Smith - A 16 Hour C# Course with Visual Studio 2013
+        //John Smith - Learn JavaScript Through Visual Studio 2013
+
+//暂时想到这么多，最后更新2018/01/15
